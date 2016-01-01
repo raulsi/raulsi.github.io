da63decd0cc7a1101f7f4518e6765d18f9859c42
Rahul Singh <raulsi07@gmail.com>
2016-01-01 20:10:03 +0530

    code cleaned

--- particle_explosion/pexplosion.js
+++ particle_explosion/pexplosion.js
@@ -118,25 +118,6 @@ function loadthreeexp(){
     window.addEventListener( 'resize', onWindowResize, false );
 
   }
-  function generateSprite() {
-
-    var canvas = document.createElement( 'canvas' );
-    canvas.width = 16;
-    canvas.height = 16;
-
-    var context = canvas.getContext( '2d' );
-    var gradient = context.createRadialGradient( canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2 );
-    gradient.addColorStop( 0, 'rgba(255,255,255,1)' );
-    gradient.addColorStop( 0.2, 'rgba(0,255,255,1)' );
-    gradient.addColorStop( 0.4, 'rgba(0,0,64,1)' );
-    gradient.addColorStop( 1, 'rgba(0,0,0,1)' );
-
-    context.fillStyle = gradient;
-    context.fillRect( 0, 0, canvas.width, canvas.height );
-
-    return canvas;
-
-  }
   document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
   	function onDocumentMouseMove( event ) {
