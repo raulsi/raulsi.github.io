154ece1195d41b1511503242be34a7bcc151754d
Rahul Singh <raulsi07@gmail.com>
2016-01-04 12:43:48 +0530

    sky added for city

--- city/city.js
+++ city/city.js
@@ -8,6 +8,8 @@ function loadthreeexp(){
   var container, stats;
   var camera, scene, renderer, particles, geometry, materials = [], parameters, i, h, color, size, particle;
   var mouseX = 0, mouseY = 0;
+  var sky, sunSphere, ms_Ocean;
+  var lastTime = (new Date()).getTime();
   var cWidth=0;
   var cHeight=0;
   var curverspeed=0.0003;
@@ -16,6 +18,67 @@ function loadthreeexp(){
 
   init();
   animate();
+  /* borrowed code from three js example */
+  function initSky() {
+
+    // Add Sky Mesh
+    sky = new THREE.Sky();
+    scene.add( sky.mesh );
+
+    // Add Sun Helper
+    sunSphere = new THREE.Mesh(
+      new THREE.SphereBufferGeometry( 20000, 16, 8 ),
+      new THREE.MeshBasicMaterial( { color: 0xffffff } )
+    );
+    sunSphere.position.y = - 700000;
+    sunSphere.visible = false;
+    scene.add( sunSphere );
+
+    /// GUI
+
+    var effectController  = {
+      turbidity: 10,
+      reileigh: 1,
+      mieCoefficient: 0.0054,
+      mieDirectionalG: 0.1,
+      luminance: 0.4,
+      inclination: 0.5, // elevation / inclination
+      azimuth: 0.1, // Facing front,
+      sun: ! true
+    };
+
+    var distance = 400000;
+
+    function guiChanged() {
+
+      var uniforms = sky.uniforms;
+      uniforms.turbidity.value = effectController.turbidity;
+      uniforms.reileigh.value = effectController.reileigh;
+      uniforms.luminance.value = effectController.luminance;
+      uniforms.mieCoefficient.value = effectController.mieCoefficient;
+      uniforms.mieDirectionalG.value = effectController.mieDirectionalG;
+
+      var theta = Math.PI * ( effectController.inclination - 0.5 );
+      var phi = 2 * Math.PI * ( effectController.azimuth - 0.5 );
+
+      sunSphere.position.x = distance * Math.cos( phi );
+      sunSphere.position.y = distance * Math.sin( phi ) * Math.sin( theta );
+      sunSphere.position.z = distance * Math.sin( phi ) * Math.cos( theta );
+
+      sunSphere.visible = effectController.sun;
+
+      sky.uniforms.sunPosition.value.copy( sunSphere.position );
+
+      renderer.render( scene, camera );
+
+    }
+
+    guiChanged();
+
+
+	}
+
+  /* borrowed code end */
 
   function createLine(vec,xRadius,yRadius,color,ocolor,lineWidth,spacePoints,opacity,name){
       var geometry = new THREE.Geometry();
@@ -36,49 +99,28 @@ function loadthreeexp(){
       scene.add(line);
   }
   function init() {
-    camera =  new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2,window.innerHeight / 2, window.innerHeight / - 2, 1, 20000 );
-    camera.position.z = 15000;
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 100, 2000000 );
+		camera.position.set( 0, 100, 2000 );
     if ( Detector.webgl )
     		renderer = new THREE.WebGLRenderer( {antialias:true} );
     else
     	  renderer = new THREE.CanvasRenderer();
+    camera.lookAt(new THREE.Vector3());
+
     scene = new THREE.Scene();
-    scene.fog = new THREE.FogExp2( 0x000000, 0.0007 );
-    scene.add(camera);
+    var helper = new THREE.GridHelper( 5000, 5000 );
+    helper.color1.setHex( 0xffffff );
+    helper.color2.setHex( 0xffffff );
+    scene.add( helper );
     renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight  );
+    renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
-    cWidth=window.innerWidth/2;
-    cHeight=window.innerHeight/2;
-    aspratio=cWidth/cHeight;
-    colorarray=['#d3d3d3','#bdbdbd','#a8a8a8','#939393','#7e7e7e','#696969','#545454','#3f3f3f','#2a2a2a','#151515']
-    crad=60;
-    var floorMaterial = new THREE.MeshBasicMaterial( {color:0xffffff } );
-  	var floorGeometry = new THREE.PlaneGeometry(2000, 1000, 32);
-  	var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-    floor.position.y=-300;
-    floor.rotation.x = Math.PI/4;
-    floor.rotation.z = 0.2;
-  	scene.add(floor);
-
-    var material = new THREE.SpriteMaterial( {
-      map: new THREE.CanvasTexture( generateSprite() ),
-      blending: THREE.AdditiveBlending
-    } );
-
-    var arx=[-windowHalfX,windowHalfX];
-    var ary=[-windowHalfY,windowHalfY];
-    for ( var i = 0; i < 2000; i++ ) {
-
-      particle = new THREE.Sprite( material );
-      particle.position.set(0,0,0);
-      particle.position.x = Math.random() *arx[parseInt((Math.random()*10) % 2)] ;
-      particle.position.y = Math.random() * ary[parseInt((Math.random()*10) % 2)]  ;
-      particle.scale.x = particle.scale.y = Math.random() * 10;
-      scene.add( particle );
-    }
-
-
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
+    controls.addEventListener( 'change', render );
+    //controls.maxPolarAngle = Math.PI / 2;
+    controls.enableZoom = false;
+    controls.enablePan = false;
+    initSky();
 
 
     stats = new Stats();
@@ -135,39 +177,7 @@ function loadthreeexp(){
     var time = Date.now() * curverspeed;
 
     camera.lookAt( scene.position );
-
-    for ( i = 0; i < scene.children.length; i ++ ) {
-
-      var object = scene.children[ i ];
-      object.rotation.z=0;
-      if ( object instanceof THREE.Line ) {
-        if(object.name=="curvecollection")
-        {
-             object.rotation.z = time * ( i % 2 ==0  ?  1 : - 1  );
-        }
-
-      }else if(object instanceof THREE.Sprite)
-      {
-
-        if(object.position.x>windowHalfX){
-          object.position.x=-windowHalfX;
-        }
-        if(object.position.x<-windowHalfX){
-          object.position.x=windowHalfX;
-        }
-        if(object.position.y>windowHalfY){
-          object.position.y=-windowHalfY;
-        }
-        if(object.position.y<-windowHalfY){
-          object.position.y=windowHalfY;
-        }
-        object.position.x=object.position.x + (Math.random() * ( i % (Math.random()*10) ==0  ?  1 : - 1  ))/10;
-        object.position.y=object.position.y + (Math.random() * ( i % (Math.random()*10) ==0  ?  1 : - 1  ))/10;
-      }else if(object instanceof THREE.Mesh){
-        //object.rotation.x = object.rotation.x + 0.1;
-      }
-
-    }
+    var currentTime = new Date().getTime();
     renderer.render( scene, camera );
   }
 
